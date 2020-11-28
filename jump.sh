c29438a595377140fad8548a7c6a0fa1b93cba07
buggsboony <boonydeboon@gmail.com>
2020-11-27 22:20:11 -0400

    version using cacheOK

--- jump.sh
+++ jump.sh
@@ -5,8 +5,70 @@ LGREEN='\033[1;32m'
 WHITE='\033[1;37m'
 YELL='\033[1;33m'
 RED='\033[0;31m'
+LRED='\033[1;31m'
+MAG='\033[0;35m'
+LMAG='\033[1;35m'
+CYAN='\033[0;36m'
+LCYAN='\033[1;36m'
 NC='\033[0m' # No Color
  
+cachedir=$HOME/.cache/jumps
+if [ ! -d $cachedir ];
+then
+    mkdir -p $cachedir       
+    if [ ! -d $cachedir ];
+    then 
+        printf "Cache directory ${LRED}'$cachedir'${NC} does not exist.${NC}\n";
+    else
+        printf "Cache directory ${LGREEN}'$cachedir'${NC} created.\n";
+    fi
+fi
+
+readcache()
+{
+     jumpname="$1"
+     path="$PWD/$2"   
+     #echo $jumpname;     
+     #echo $path;
+     if [ "$path" == "" ]; then
+        printf "";#Not found
+     else
+        cachefile="$cachedir/$jumpname"
+        if [ -f "$cachefile" ]; then
+            #get to content             
+            target=$(cat "$cachefile")
+        else
+            target="";
+        fi     
+     fi
+     echo $target
+}
+
+writecache()
+{
+    if [ "$target" == "" ];then   
+        printf "";
+    else
+            jumpname="$1" 
+            path="$PWD/$2"               
+            # echo JUMPNAME $jumpname;     
+            # echo PATH $path;
+            # echo CURRENT $PWD;
+            if [ "$path" == "" ]; then
+                printf "will not create an empty file";#Not found
+            else            
+                cachefile="$cachedir/$jumpname"
+                if [ -f "$cachefile" ]; then
+                    #jump to content             
+                    echo "jump to content";
+                else
+                    # Create content
+                    echo "$path">"$cachefile";
+                    printf "Cache file ${LGREEN}'$jumpname'${NC} created.\n";  
+                fi     
+            fi
+     fi
+}
 
 
 ##version 1, iname :
@@ -15,14 +77,31 @@ NC='\033[0m' # No Color
 
 #target=$(find $PWD -type d -iwholename "*/$1" | head -n 1)  #ptet un probleme quand ya un dossier est symblink
 
-cd $PWD
-target=$(find . -type d -iwholename "*/$1" | head -n 1)
+#cd $PWD
 
- 
 
-printf "Jumping to ${YELL}'${target}'${NC}\n"
+target=$(readcache "$1")
 
-cd "$target"
+
+if [ "$target" == "" ];then   
+    target=$(find . -type d -iwholename "*/$1" | head -n 1)        
+    writecache "$1" "$target"
+fi
+
+#
+
+#Read cache
+#writecache "$1" "$target"
+
+
+
+if [ "$target" == "" ];then  
+   printf "Oups, nothing found...\n";
+else
+   printf "Jumping to ${YELL}'${target}'${NC}\n"
+   cd "$target"
+fi
+
 
 ##source ${BASH_SOURCE[0]}
 ##Ceci fonctionne
