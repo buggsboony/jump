e3bc2808c7f8d5014c292f9ee3f34739411afce0
buggsboony <boonydeboon@gmail.com>
2021-09-19 15:31:33 -0400

    must copy to .zshrc

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ chmod +x $what$extension
 echo "Adding alias in .bashrc"
 echo  "" >> $HOME/.bashrc
 echo  "### Jump alias generated code at $(date +"%Y-%m-%d %H:%I:%S" )" >> $HOME/.bashrc
-echo  "jump( ) { source $PWD/$what.sh \$*; }" >> $HOME/.bashrc
+echo  "jump() { source $PWD/$what.sh \$*; }" >> $HOME/.bashrc
  
 #######################################################   completion parameters #########################
 #enable autocompletion for jump parameter
